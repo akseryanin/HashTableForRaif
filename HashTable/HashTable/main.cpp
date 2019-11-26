d994c48af3a879562220de1b74ee30549da5e3c7
akseryanin <sasha.seryanin@yandex.ru>
2019-11-26 21:26:40 +0300

    Пояснения в ридми и еще комменты

--- HashTable/HashTable/main.cpp
+++ HashTable/HashTable/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-	//смотрите юнит тесты и другие файлы)
-	cout << "Hello, raiffeisen";
+	//смотрите юнит тесты, readme и другие файлы)
+	cout << "Hello, raiffeisen!";
 	return 0;
 }
